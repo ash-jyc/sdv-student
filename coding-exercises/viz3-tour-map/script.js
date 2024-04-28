cc712a9b8bc05ee7aac6753d4c354e0c272541d9
Ashley <ajychen26@gmail.com>
2024-04-29 01:54:14 +0800

    coldplay tour

--- coding-exercises/viz3-tour-map/script.js
+++ coding-exercises/viz3-tour-map/script.js
@@ -28,53 +28,48 @@ let viz = d3.select("#container")
 // mosaic of album in a country??
 // labelling countries because i don't know my geography
 
-let artists = ["Coldplay"] // , "Olivia Rodrigo", "Beyonce", "Mitski", "Travis Scott", "Doja Cat", "Justin Timberlake", "Bad Bunny", "Adele", "J. Cole", "Nicki Minaj", "P!NK"]
-let colors = ["red"] //, "blue", "green", "purple", "orange", "yellow", "pink", "brown", "black", "magenta", "cyan", "white"]
+// let artists = ["Coldplay"] // , "Olivia Rodrigo", "Beyonce", "Mitski", "Travis Scott", "Doja Cat", "Justin Timberlake", "Bad Bunny", "Adele", "J. Cole", "Nicki Minaj", "P!NK"]
+// let colors = ["red"] //, "blue", "green", "purple", "orange", "yellow", "pink", "brown", "black", "magenta", "cyan", "white"]
 let spotifyCountries = [
     "AE", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "BY", "CA", "CH", "CL", "CO", "CR", "CZ", "DE", "DK", "DO", "EC", "EE", "EG", "ES", "FI", "FR", "GB", "GR", "GT", "HK", "HN", "HU", "ID", "IE", "IL", "IN", "IS", "IT", "JP", "KR", "KZ", "LT", "LU", "LV", "MA", "MX", "MY", "NG", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PK", "PL", "PT", "PY", "RO", "SA", "SE", "SG", "SK", "SV", "TH", "TR", "TW", "UA", "US", "UY", "VE", "VN", "ZA"
 ];
+
 let tourDetails = [
-    { "date": "2023-09-27", "country": "US" },
-    { "date": "2023-09-28", "country": "US" },
-    { "date": "2023-11-11", "country": "TW" },
-    { "date": "2023-11-12", "country": "TW" },
-    { "date": "2023-11-15", "country": "ID" },
-    { "date": "2023-11-18", "country": "AU" },
-    { "date": "2023-11-19", "country": "AU" },
-    { "date": "2023-11-22", "country": "MY" },
-    { "date": "2024-01-19", "country": "PH" },
-    { "date": "2024-01-20", "country": "PH" },
-    { "date": "2024-01-23", "country": "SG" },
-    { "date": "2024-01-27", "country": "SG" },
-    { "date": "2024-01-23", "country": "SG" },
-    { "date": "2024-01-24", "country": "SG" },
-    { "date": "2024-01-25", "country": "SG" },
-    { "date": "2024-01-26", "country": "SG" },
-    { "date": "2024-01-27", "country": "SG" },
-    { "date": "2024-01-23", "country": "SG" },
-    { "date": "2024-01-24", "country": "SG" },
-    { "date": "2024-01-25", "country": "SG" },
-    { "date": "2024-01-26", "country": "SG" },
-    { "date": "2024-01-27", "country": "SG" },
-    { "date": "2024-02-03", "country": "TH" },
-    { "date": "2024-02-04", "country": "TH" },
+    { "date": "2023-09-27", "country": "US", "city": "Los Angeles", "lat": 34.0522, "lng": -118.2437 },
+    { "date": "2023-09-28", "country": "US", "city": "Los Angeles", "lat": 34.0522, "lng": -118.2437 },
+    { "date": "2023-11-11", "country": "TW", "city": "Kaohsiung", "lat": 22.6273, "lng": 120.3014 },
+    { "date": "2023-11-12", "country": "TW", "city": "Kaohsiung", "lat": 22.6273, "lng": 120.3014},
+    { "date": "2023-11-15", "country": "ID", "city": "Jakarta", "lat": -6.2088, "lng": 106.8456 },
+    { "date": "2023-11-18", "country": "AU", "city": "Perth", "lat": -31.9505, "lng": 115.8605 },
+    { "date": "2023-11-19", "country": "AU", "city": "Perth", "lat": -31.9505, "lng": 115.8605 },
+    { "date": "2023-11-22", "country": "MY", "city": "Kuala Lumpur", "lat": 3.1390, "lng": 101.6869 },
+    { "date": "2024-01-19", "country": "PH", "city": "Manila", "lat": 14.5995, "lng": 120.9842 },
+    { "date": "2024-01-20", "country": "PH", "city": "Manila", "lat": 14.5995, "lng": 120.9842 },
+    { "date": "2024-01-23", "country": "SG", "city": "Kallang", "lat": 1.3114, "lng": 103.8822},
+    { "date": "2024-01-24", "country": "SG", "city": "Kallang", "lat": 1.3114, "lng": 103.8822 },
+    { "date": "2024-01-25", "country": "SG", "city": "Kallang", "lat": 1.3114, "lng": 103.8822 },
+    { "date": "2024-01-26", "country": "SG", "city": "Kallang", "lat": 1.3114, "lng": 103.8822},
+    { "date": "2024-01-27", "country": "SG", "city": "Kallang", "lat": 1.3114, "lng": 103.8822 },
+    { "date": "2024-02-03", "country": "TH", "city": "Bangkok", "lat": 13.7563, "lng": 100.5018},
+    { "date": "2024-02-04", "country": "TH", "city": "Bangkok", "lat": 13.7563, "lng": 100.5018},
 ]
 d3.json("custom.geo.json").then(function (geoData) {
-    d3.csv("../universal_top_spotify_songs.csv").then(function (incomingData) {
+    d3.csv("./coldplay-data.csv").then(function (incomingData) {
         console.log("geoData", geoData);
 
         let tourData = incomingData.filter(d => {
             let songArtists = d.artists.split(",")
-            // for (let i = 0; i < artists.length; i++) {
             if (songArtists.includes("Coldplay")) {
                 d.artists = "Coldplay";
                 return true;
             }
-            // }
-            // return false;
         })
 
+        console.log("coldplay data")
+        console.log(tourData)
+
         let timeParser = d3.timeParse("%Y-%m-%d")
+        let timeFormatter = d3.timeFormat("%Y-%m-%d")
 
         function mapFunction(d) {
             d.snapshot_date = timeParser(d.snapshot_date);
@@ -83,11 +78,14 @@ d3.json("custom.geo.json").then(function (geoData) {
         }
 
         // filter dates in dataset
-        let dataTourDates = tourData.filter(d => {
-            let tourDates = tourDetails.map(e => e.date)
-            return tourDates.includes(d.snapshot_date)
-        })
+        let dataTourDates = tourData.filter(d => d.album_release_date !== "N/A");
+        tourDetails = tourDetails.map(d => {
+            d.date = timeParser(d.date);
+            return d;
+        });
 
+        console.log("tours with dates")
+        console.log(tourDetails)
         console.log(dataTourDates)
 
         let filteredDataWithTime = dataTourDates.map(mapFunction);
@@ -95,53 +93,23 @@ d3.json("custom.geo.json").then(function (geoData) {
             d.album_release_date < timeParser("2020-01-01")
         );
 
-        let groupedData = d3.groups(filteredDataWithTime, function (d) {
-            return d.artists;
-        })
-
-        // make a list of countries artist toured at
-        // let artistsTourCountries = groupedData.map(d => {
-        //     let countries = d[1].map(e => e.country)
-        //     return { artist: d[0], countries: countries }
-        // });
-
-        // console.log(artistsTourCountries)
-
-        // // color countries based on artist
-        // let countryColor = {}
-        // artistsTourCountries.forEach(d => {
-        //     d.countries.forEach(e => {
-        //         countryColor[e] = d.artist
-        //     })
-        // })
-
-        // let artistToColor = {}
-        // for (let i = 0; i < artists.length; i++) {
-        //     artistToColor[artists[i]] = colors[i]
-        // }
-
-        // console.log(countryColor)
-        // console.log(artistToColor)
+        let groupedData = d3.group(dataTourDates, d => timeFormatter(d.snapshot_date));
+        // check that date is in tour
+        console.log("groupedData")
+        console.log(groupedData)
 
         // draw map
-        let projection = d3.geoEqualEarth();
+        let projection = d3.geoMercator()
+            .center([130, -15])
+            .scale(425)
+            .translate([w / 2, h / 2]); 
 
         let pathMaker = d3.geoPath(projection);
         viz.selectAll(".country").data(geoData.features).enter()
             .append("path")
             .attr("class", "country")
             .attr("d", pathMaker)
-            .attr("fill", /*function (d, i) {
-                // console.log(d.properties.iso_a2)
-                let country = d.properties.iso_a2
-                // console.log(country, countryColor[country], artistToColor[countryColor[country]])
-                if (countryColor[country]) {
-                    console.log(country, countryColor[country], artistToColor[countryColor[country]])
-                    return artistToColor[countryColor[country]]
-                } else {
-                    return "gray"
-                }
-            })*/ "gray")
+            .attr("fill", "gray")
             .attr("opacity", function (d, i) {
                 if (spotifyCountries.includes(d.properties.iso_a2)) {
                     return 1
@@ -152,22 +120,79 @@ d3.json("custom.geo.json").then(function (geoData) {
             .attr("stroke", "white")
         
         function updateMapForDate(tourDate) {
-            let activeCountries = tourDetails.filter(d => d.date === tourDate);
-            let activeCountryCodes = activeCountries.map(d => d.country);
-            
+            let currentData = groupedData.get(timeFormatter(tourDate));
+            console.log("currentData", currentData)
+            // make current tour country a different color
+            let activeCountryCodes = currentData.map(d => d.country);
+            console.log("activeCountryCodes", activeCountryCodes)
+
             viz.selectAll(".country")
-                .transition()
-                .duration(200)
-                .attr("fill", d => activeCountryCodes.includes(d.properties.iso_a2) ? "red" : "lightgray");
+                .attr("fill", function (d, i) {
+                    if (tourDetails.find(t => t.date === tourDate).country === d.properties.iso_a2){
+                        return "pink"
+                    } else if (activeCountryCodes.includes(d.properties.iso_a2)) {
+                        return "blue"
+                    } else {
+                        return "gray"
+                    }
+                })
+                .attr("opacity", function (d, i) {
+                    if (activeCountryCodes.includes(d.properties.iso_a2)) {
+                        return 1
+                    } else {
+                        return 0.2
+                    }
+                })
+
+            
+            // drop a pin for the city of the tour date
+            let tourCity = tourDetails.find(d => d.date === tourDate);
+            viz.selectAll(".tour-city").remove();
+            viz.append("circle")
+                .attr("class", "tour-city")
+                .attr("cx", projection([tourCity.lng, tourCity.lat])[0])
+                .attr("cy", projection([tourCity.lng, tourCity.lat])[1])
+                .attr("r", 5)
+                .attr("fill", "red")
+                .attr("opacity", 0.5)
+            
+            viz.append("text")
+                .attr("class", "tour-city")
+                .attr("x", projection([tourCity.lng, tourCity.lat])[0])
+                .attr("y", projection([tourCity.lng, tourCity.lat])[1])
+                .text(tourCity.city)
+                .attr("font-size", 10)
+                .attr("font-family", "sans-serif")
+                .attr("text-anchor", "middle")
+                .attr("alignment-baseline", "middle")
+                .attr("fill", "black")
+                .attr("opacity", 0.2)
+            
+            // append date
+            viz.selectAll(".tour-date").remove();
+            viz.append("text")
+                .attr("class", "tour-date")
+                .attr("x", w - xPadding)
+                .attr("y", h - yPadding)
+                .text(timeFormatter(tourDate))
+                .attr("font-size", 20)
+                .attr("font-family", "sans-serif")
+                .attr("text-anchor", "end")
+                .attr("alignment-baseline", "middle")
+                .attr("fill", "black")
+                .attr("opacity", 0.2)
         }
 
-        let sortedTourDates = Array.from(new Set(tourDetails.map(d => d.date))).sort();
-
+        let sortedTourDates = Array.from(new Set(tourDetails.map(d => d.date))).sort((a, b) => a - b);
+        console.log("sortedTourDates", sortedTourDates)
         function animateTours() {
             let i = 0;
             let interval = d3.interval(function() {
                 if (i >= sortedTourDates.length) interval.stop();
-                updateMapForDate(sortedTourDates[i]);
+                // check if in groupedData
+                if (groupedData.has(timeFormatter(sortedTourDates[i]))) {
+                    updateMapForDate(sortedTourDates[i]);
+                }
                 i++;
             }, 1000); // Update every second
         }
